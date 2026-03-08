9624e9f5c6ce28a694b22575f5a4f9cf641822ec
AntaBalende <antabalende5@gmail.com>
2026-03-08 20:09:56 +0100

    Ajout Timer et score

--- src/pages/Score.jsx
+++ src/pages/Score.jsx
@@ -1,4 +1,5 @@
 import { useLocation, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function Score(){
 
@@ -7,16 +8,48 @@ function Score(){
 
  const score = location.state?.score || 0;
 
- let message = "";
+ const [name,setName] = useState("");
+ const [bestScores,setBestScores] = useState([]);
 
- if(score <= 4){
-  message = "Pas génial là !";
+ useEffect(()=>{
+
+  const storedScores = JSON.parse(localStorage.getItem("scores")) || [];
+
+  setBestScores(storedScores);
+
+ },[]);
+
+ function saveScore(){
+
+  let scores = JSON.parse(localStorage.getItem("scores")) || [];
+
+  const newEntry = {
+   name: name || "Player",
+   score: score
+  };
+
+  scores.push(newEntry);
+
+  scores.sort((a,b)=> b.score - a.score);
+
+  scores = scores.slice(0,10);
+
+  localStorage.setItem("scores", JSON.stringify(scores));
+
+  setBestScores(scores);
+
  }
- else if(score <= 7){
-  message = "Ça commence à venir";
+
+ let message="";
+
+ if(score<=4){
+  message="Pas génial là !";
+ }
+ else if(score<=7){
+  message="Ça commence à venir";
  }
  else{
-  message = "Bravo !";
+  message="Bravo !";
  }
 
  return(
@@ -28,6 +61,29 @@ function Score(){
    <h2>Score : {score} / 10</h2>
 
    <p>{message}</p>
+
+   <h3>Entrer votre pseudo</h3>
+
+   <input
+    type="text"
+    placeholder="Votre nom"
+    value={name}
+    onChange={(e)=>setName(e.target.value)}
+   />
+
+   <button onClick={saveScore}>
+    Enregistrer score
+   </button>
+
+   <h2>Top 10 Scores</h2>
+
+   <ol>
+    {bestScores.map((s,i)=>(
+     <li key={i}>
+      {s.name} - {s.score} / 10
+     </li>
+    ))}
+   </ol>
 
    <button onClick={()=>navigate("/")}>
     Rejouer
